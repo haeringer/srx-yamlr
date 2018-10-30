8965b914550e44ee777b91378d17b14bfa884549
Ben Haeringer <ben.haeringer@gmail.com>
2018-10-30 11:48:01 +0100

    create index view

---
+++ generator/views.py
@@ -0,0 +1,16 @@
+yamlfile = 'kami-test.yml'
+
+from django.shortcuts import render
+
+from .models import SrxZone, SrxAddress, SrxAddrSet, SrxApplication, \
+    SrxAppSet, SrxPolicy, SrxProtocol
+
+from generator.helpers import importyaml
+
+# call function from helpers.py to import YAML data
+importyaml(yamlfile)
+
+def index(request):
+    zones = SrxZone.objects.all()
+    context = {'zones': zones}
+    return render(request, 'generator/index.html', context)
