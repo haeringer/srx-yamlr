c757c907872af6dff813bec06b9e31df60cfc32b
Ben Haeringer <ben.haeringer@gmail.com>
2018-10-30 11:47:11 +0100

    create database structure

---
+++ generator/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+
+class SrxZone(models.Model):
+    zone_name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.zone_name
+
+class SrxAddress(models.Model):
+    zone = models.ForeignKey(SrxZone, on_delete=models.CASCADE)
+    address_name = models.CharField(max_length=255)
+    address_ip = models.CharField(max_length=255)
+    def __str__(self):
+        return self.address_name
+
+class SrxAddrSet(models.Model):
+    zone = models.ForeignKey(SrxZone, on_delete=models.CASCADE)
+    addrset_name = models.CharField(max_length=255)
+    address = models.ManyToManyField(SrxAddress)
+    def __str__(self):
+        return self.addrset_name
+
+class SrxProtocol(models.Model):
+    protocol_type = models.CharField(max_length=3)
+    def __str__(self):
+        return self.protocol_type
+
+class SrxApplication(models.Model):
+    application_name = models.CharField(max_length=255)
+    protocol = models.ForeignKey(SrxProtocol, on_delete=models.PROTECT, default=0)
+    application_port = models.IntegerField()
+    def __str__(self):
+        return self.application_name
+
+class SrxAppSet(models.Model):
+    applicationset_name = models.CharField(max_length=255)
+    applications = models.ManyToManyField(SrxApplication)
+    def __str__(self):
+        return self.applicationset_name
+
+class SrxPolicy(models.Model):
+    policy_name = models.CharField(max_length=255)
+    from_zone = models.ManyToManyField(SrxZone, related_name='from_zones')
+    to_zone = models.ManyToManyField(SrxZone, related_name='to_zones')
+    source_address = models.ManyToManyField(SrxAddress, related_name='source_addresses')
+    # plus address sets ??
+    destination_address = models.ManyToManyField(SrxAddress, related_name='destination_addresses')
+    # plus address sets ??
+    applications = models.ManyToManyField(SrxApplication)
+    # plus application sets ??
+    def __str__(self):
+        return self.policy_name
