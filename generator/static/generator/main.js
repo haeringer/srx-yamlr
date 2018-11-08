9671f4dad9eafb36c7211e1647a2c052cd70f5ed
Ben Haeringer <ben.haeringer@gmail.com>
2018-11-08 11:58:34 +0100

    clear input fields on click

--- generator/static/generator/main.js
+++ generator/static/generator/main.js
@@ -7,9 +7,14 @@ $(function() {
     /* Recognize clicks on search result item */
     // use .on 'click' with parent selected to recognize events also on newly added items
     $('.list-inline').on('click', '.search-results-item', function(e) {
-        var name = this.textContent;
-        addObject(this, name);
+        var objText = this.textContent;
+        var objectId_dj = this.id.split("_").pop();
+        var source = this.id.split("_").shift();
+
+        addObject(objText, objectId_dj, source);
+
         $(e.target).closest('.list-inline').addClass('d-none');
+        $('#search-'+source).val('')
     });
 
 });
@@ -17,20 +22,16 @@ $(function() {
 
 /* After click on search result item (see event listener),
 retrieve parent zone and add object to card element */
-function addObject(obj, objName) {
-    var objectId = obj.id;
-    var objectId_dj = objectId.split("_").pop()
-    var source = objectId.split("_").shift()
-    console.log(objectId);
-
+function addObject(objText, objectId_dj, source) {
     if (source === 'from' || source === 'to') {
+        // ajax call to retrieve parent zone
         $.getJSON('/generator/getparentzone/', {objectid: objectId_dj})
 
         .done(function(response) {
             // blend in card element
             $('#added-obj-'+source).removeClass('d-none');
             $('#added-zone-'+source).html(response);
-            $('#added-list-'+source).append(`<li class="list-group-item">${objName}</li>`);
+            $('#added-list-'+source).append(`<li class="list-group-item">${objText}</li>`);
         })
 
         .fail(function(errorThrown) {
@@ -38,7 +39,7 @@ function addObject(obj, objName) {
         });
     } else if (source === 'app') {
         $('#added-obj-app').removeClass('d-none');
-        $('#added-list-app').append(`<li class="list-group-item">${objName}</li>`);
+        $('#added-list-app').append(`<li class="list-group-item">${objText}</li>`);
     }
 }
 
