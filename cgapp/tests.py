6c4e4d1ba2d92b4d1dfa959152b17b5af816caea
Ben Haeringer <ben.haeringer@gmail.com>
2019-01-31 16:05:37 +0100

    implement unit testing

--- cgapp/tests.py
+++ cgapp/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from .views import *
 
-# Create your tests here.
+
+
+username = 'testuser'
+password = '123456'
+
+
+class viewTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+
+        user = User.objects.create(username=username)
+        user.set_password(password)
+        user.save()
+
+        c = Client()
+        c.get('/ajax/loadobjects/', {'loadpolicies': 'False'})
+        c.get('/ajax/loadobjects/', {'loadpolicies': 'True'})
+
+
+    def setUp(self):
+
+        logged_in = self.client.login(username=username, password=password)
+        self.assertIs(logged_in, True)
+
+
+    def test_login_page_plus_redirect(self):
+
+        response = self.client.post('/auth/login/', {
+            'username': username, 'password': password},
+        )
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_main_page_load(self):
+
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
