8f668cd57eb99b6e8e2f99d2dfac70acdcaa1a90
Ben Haeringer <ben.haeringer@gmail.com>
2019-02-01 16:24:56 +0100

    add test objects to unit tests

--- cgapp/tests.py
+++ cgapp/tests.py
@@ -1,3 +1,4 @@
+import uuid
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from .views import *
@@ -21,11 +22,56 @@ class viewTests(TestCase):
         c.get('/ajax/loadobjects/', {'loadpolicies': 'False'})
         c.get('/ajax/loadobjects/', {'loadpolicies': 'True'})
 
+        c.login(username=username, password=password)
+        c.get('/')
 
-    def setUp(self):
+        z = SrxZone.objects.filter()
+        zone = z[0].name
 
-        logged_in = self.client.login(username=username, password=password)
-        self.assertIs(logged_in, True)
+        for i in range(3):
+            c.post('/ajax/newobject/', {
+                'objtype': 'address',
+                'zone': zone,
+                'name': 'TEST_ADDRESS_{}'.format(i),
+                'value': '10.20.30.{}/32'.format(i),
+                },
+            )
+        c.post('/ajax/newobject/', {
+            'objtype': 'addrset',
+            'zone': zone,
+            'name': 'TEST_ADDRSET',
+            'valuelist[]': ['TEST_ADDRESS_0','TEST_ADDRESS_1'],
+            },
+        )
+        for i in range(3):
+            c.post('/ajax/newobject/', {
+                'objtype': 'application',
+                'name': 'TEST_APPLICATION_{}'.format(i),
+                'port': '123{}'.format(i),
+                'protocol': 'tcp',
+                },
+            )
+        c.post('/ajax/newobject/', {
+            'objtype': 'appset',
+            'name': 'TEST_APPSET',
+            'valuelist[]': ['TEST_APPLICATION_0','TEST_APPLICATION_1'],
+            },
+        )
+
+
+    def test_for_created_test_objects(self):
+
+        a = SrxAddress.objects.filter().get(name='TEST_ADDRESS_0')
+        self.assertEqual(a.name, 'TEST_ADDRESS_0')
+
+        a = SrxAddrSet.objects.filter().get(name='TEST_ADDRSET')
+        self.assertEqual(a.name, 'TEST_ADDRSET')
+
+        a = SrxApplication.objects.filter().get(name='TEST_APPLICATION_0')
+        self.assertEqual(a.name, 'TEST_APPLICATION_0')
+
+        a = SrxAppSet.objects.filter().get(name='TEST_APPSET')
+        self.assertEqual(a.name, 'TEST_APPSET')
 
 
     def test_login_page_plus_redirect(self):
@@ -34,9 +80,3 @@ class viewTests(TestCase):
             'username': username, 'password': password},
         )
         self.assertEqual(response.status_code, 302)
-
-
-    def test_main_page_load(self):
-
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
