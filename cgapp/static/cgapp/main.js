04155f3fcf75b0b9c574bb50903bfe721ccf848f
Ben Haeringer <ben.haeringer@gmail.com>
2019-02-13 13:17:39 +0100

    Use simpler ID than uuid4 for policies

--- cgapp/static/cgapp/main.js
+++ cgapp/static/cgapp/main.js
@@ -3,8 +3,7 @@
 * Run on page load
 */
 $(window).on('load', function() {
-    // generate an initial configuration id
-    var newID = uuidv4();
+    var newID = generateId();
     currentObj['policyid'] = newID;
 });
 
@@ -582,11 +581,8 @@ function resetSearch(item) {
 }
 
 
-/* UUID cgapp helper function */
-function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    )
+function generateId() {
+    return Math.random().toString(36).substr(2, 9)
 }
 
 /* Check if arrays inside object are empty helper function */
