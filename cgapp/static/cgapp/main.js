e86b6ddfb4534e792ad3cbd74d68f73c64d69e73
Ben Haeringer <ben.haeringer@gmail.com>
2019-02-13 13:16:38 +0100

    Make ajax POST requests work with Django's CSRF protection

--- cgapp/static/cgapp/main.js
+++ cgapp/static/cgapp/main.js
@@ -8,6 +8,31 @@ $(window).on('load', function() {
     currentObj['policyid'] = newID;
 });
 
+// Make ajax POST requests work with Django's CSRF protection
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        function getCookie(name) {
+            var cookieValue = null;
+            if (document.cookie && document.cookie != '') {
+                var cookies = document.cookie.split(';');
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = jQuery.trim(cookies[i]);
+                    // Does this cookie string begin with the name we want?
+                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
+        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+            // Only send the token to relative URLs i.e. locally.
+            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+        }
+    }
+});
+
 /*
 * Event listeners etc. - run once DOM is ready
 */
