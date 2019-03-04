620bd17c4872b31ed3a51625ce9cde1ebbb1c469
Ben Haeringer <ben.haeringer@gmail.com>
2019-03-04 15:53:13 +0100

    Implement Jenkins job start + status polling

--- srxapp/consumers.py
+++ srxapp/consumers.py
@@ -1,24 +1,73 @@
-from channels.generic.websocket import WebsocketConsumer
 import json
-import subprocess as sp
+import time
+import difflib
+import logging
+
+from channels.generic.websocket import WebsocketConsumer
+from srxapp.utils import helpers
+
+logger = logging.getLogger(__name__)
 
 
-class CheckConsumer(WebsocketConsumer):
+class SrxappWebsocketConsumer(WebsocketConsumer):
+
+    def connect(self):
+        self.accept()
+        self.user = self.scope["user"]
+
+    def send(self, message):
+        super().send(text_data=json.dumps({'message': message}))
+
+
+class CheckConsumer(SrxappWebsocketConsumer):
 
     def receive(self, text_data):
-        cmd = ['ping', '-c 5', '10.13.0.1']
-        cmdstr = ''
-        for c in cmd:
-            cmdstr = cmdstr + c + ' '
+        job_url = helpers.get_jenkins_job_url(commit='no')
+        session = helpers.get_jenkins_session_object()
 
-        self.send(text_data=json.dumps({'message': cmdstr+'\n'}))
+        # Activate Jenkins by calling the job url. Jenkins then will put it
+        # into the building queue.
+        job_request = session.post(job_url)
+        if job_request.status_code != 201:
+            error = 'Jenkins job request failed:\n{}'.format(job_request)
+            self.send(error)
+            logger(error)
+            return
+        queue_url = job_request.headers['Location']
 
-        proc = sp.Popen(cmd, stdout=sp.PIPE, universal_newlines=True)
-        for output_line in proc.stdout:
-            self.send(text_data=json.dumps({'message': output_line}))
+        # Poll Jenkins until it provides a url for the build
+        self.send('Starting Jenkins job')
+        while True:
+            queue_request = session.get('{}api/json/'.format(queue_url))
+            queue_json = queue_request.json()
+            if 'executable' in queue_json:
+                self.send('\n')
+                build_url = queue_json['executable']['url']
+                break
+            time.sleep(1)
+            self.send('.')
+
+        # Poll Jenkins for build output
+        logger.info('Starting build at {}'.format(build_url))
+        diff_data = ''
+        while True:
+            build_request = session.get('{}api/json/'.format(build_url))
+            console_out = session.get('{}consoleText'.format(build_url))
+
+            diff = difflib.context_diff(diff_data, console_out.text)
+            delta = ''.join(l[2:] for l in diff if l.startswith('+ '))
+            diff_data = '{}{}'.format(diff_data, delta)
+
+            if delta != '':
+                self.send(delta)
+
+            build_json = build_request.json()
+            if build_json['building'] is False:
+                break
+            time.sleep(1)
 
 
-class DeploymentConsumer(WebsocketConsumer):
+class DeploymentConsumer(SrxappWebsocketConsumer):
 
     def receive(self, text_data):
         pass
