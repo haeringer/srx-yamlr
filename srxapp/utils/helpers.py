26015450b829ff2919ae7464bec19f1b864d615f
Ben Haeringer <ben.haeringer@gmail.com>
2019-07-04 15:55:25 +0200

    Move git functions into separate module

--- srxapp/utils/helpers.py
+++ srxapp/utils/helpers.py
@@ -1,6 +1,5 @@
 import os
 import json
-import git
 import logging
 import traceback
 
@@ -12,25 +11,6 @@ from base64 import b64encode, b64decode
 logger = logging.getLogger(__name__)
 yaml = YAML()
 yaml.indent(mapping=2, sequence=4, offset=2)
-
-
-def git_clone_to_workspace():
-    git_url = os.environ.get('YM_GIT_URL', '')
-
-    # abuse try/except for logic because git.Repo does not
-    # provide proper return values if it doesn't succeed
-    try:
-        repo = git.Repo('workspace')
-    except Exception:
-        repo = None
-
-    if repo:
-        logger.info('Updating repo...')
-        remote_repo = repo.remotes.origin
-        remote_repo.pull()
-    else:
-        logger.info('Cloning repo...')
-        git.Repo.clone_from(git_url, 'workspace')
 
 
 def view_exception(Exception):
