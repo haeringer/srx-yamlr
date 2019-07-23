befdbb3873a77a6d94350328ab96a85762cf7280
Ben Haeringer <ben.haeringer@gmail.com>
2019-07-23 17:35:54 +0200

    Require email address at user creation

--- srxapp/models.py
+++ srxapp/models.py
@@ -1,3 +1,6 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -18,3 +21,30 @@ def create_user_settings(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_settings(sender, instance, **kwargs):
     instance.usersettings.save()
+
+
+class EmailRequiredMixin(object):
+    def __init__(self, *args, **kwargs):
+        super(EmailRequiredMixin, self).__init__(*args, **kwargs)
+        self.fields['email'].required = True
+
+
+class MyUserCreationForm(EmailRequiredMixin, UserCreationForm):
+    pass
+
+
+class MyUserChangeForm(EmailRequiredMixin, UserChangeForm):
+    pass
+
+
+class EmailRequiredUserAdmin(UserAdmin):
+    form = MyUserChangeForm
+    add_form = MyUserCreationForm
+    add_fieldsets = ((None, {
+        'fields': ('username', 'email', 'password1', 'password2'),
+        'classes': ('wide',)
+    }),)
+
+
+admin.site.unregister(User)
+admin.site.register(User, EmailRequiredUserAdmin)
