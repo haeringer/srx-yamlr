39ee817a1416660bc2a4844dd49c2ca57954fac2
Ben Haeringer <ben.haeringer@gmail.com>
2019-07-23 13:56:30 +0200

    Add STATIC_ROOT for collection of static files

--- settings.py
+++ settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = "/var/www/srx-yamlr/static/"
 
 LOGIN_URL = "/auth/login"
 LOGOUT_URL = "/auth/logout"
