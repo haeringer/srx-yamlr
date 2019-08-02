808bba3636e0607428eb701deadbb1c7e5af9dfd
Ben Haeringer <ben.haeringer@gmail.com>
2019-08-02 16:07:17 +0200

    Move static dir from app to project level

--- yamlomatapp/main/settings.py
+++ yamlomatapp/main/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "/var/www/srx-yamlr/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 LOGIN_URL = "/auth/login"
 LOGOUT_URL = "/auth/logout"
