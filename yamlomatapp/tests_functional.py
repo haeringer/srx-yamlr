4006702333144e6a13a6bc0af22d63eaae0149ae
Ben Haeringer <ben.haeringer@gmail.com>
2019-08-20 12:52:33 +0200

    Improve Selenium Integration + implement basic tests

--- yamlomatapp/tests_functional.py
+++ yamlomatapp/tests_functional.py
@@ -1,44 +1,45 @@
-from django.test import TestCase
-# from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import socket
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from django.contrib.auth.models import User
 
 
-class SeleniumTest(TestCase):
+class MySeleniumTests(StaticLiveServerTestCase):
+    host = "0.0.0.0"
 
-    def setUp(self):
-        self.browser = webdriver.Remote(
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.host = socket.gethostbyname(socket.gethostname())
+        cls.browser = webdriver.Remote(
             command_executor='http://selenium_chrome:4444/wd/hub',
             desired_capabilities=DesiredCapabilities.CHROME
         )
-        self.browser.implicitly_wait(10)
+        cls.browser.implicitly_wait(5)
 
-    def test_visit_site_with_browser(self):
-        self.browser.get('http://web:8000')
-        self.assertIn("SRX YAMLr", self.browser.title)
+        User.objects.create_user(
+            username="functest_user",
+            email="functest_user@example.com",
+            password="123456"
+        )
 
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+        super().tearDownClass()
 
-# class MySeleniumTests(StaticLiveServerTestCase):
-#     fixtures = ['user-data.json']
+    def test_login_fail(self):
+        self.browser.get('%s%s' % (self.live_server_url, ''))
+        self.browser.find_element_by_id("id_username").send_keys('functest_user')
+        self.browser.find_element_by_id("id_password").send_keys('invalid')
+        self.browser.find_element_by_class_name("btn-block").click()
+        error_class = self.browser.find_element_by_class_name("errorlist")
+        self.assertIn("Please enter a correct username and password", error_class.text)
 
-#     @classmethod
-#     def setUpClass(cls):
-#         super().setUpClass()
-#         cls.browser = webdriver.Remote(
-#             command_executor='http://selenium_hub:4444/wd/hub',
-#             desired_capabilities=DesiredCapabilities.CHROME
-#         )
-#         cls.browser.implicitly_wait(10)
-
-#     @classmethod
-#     def tearDownClass(cls):
-#         cls.browser.quit()
-#         super().tearDownClass()
-
-#     def test_login(self):
-#         self.browser.get('%s%s' % (self.live_server_url, '/login/'))
-#         username_input = self.browser.find_element_by_name("username")
-#         username_input.send_keys('admin')
-#         password_input = self.browser.find_element_by_name("password")
-#         password_input.send_keys('ifmRos=')
-#         self.browser.find_element_by_xpath('//input[@value="Log in"]').click()
+    def test_login(self):
+        self.browser.get('%s%s' % (self.live_server_url, ''))
+        self.browser.find_element_by_id("id_username").send_keys('functest_user')
+        self.browser.find_element_by_id("id_password").send_keys('123456')
+        self.browser.find_element_by_class_name("btn-block").click()
